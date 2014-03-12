29ef372297251818c09e6fcf235b1fc8ee310ca8
bhanupavan <bhanupavansingh@gmail.com>
2014-03-12 17:52:46 +0530

    commit 12

--- merge.py
+++ merge.py
@@ -8,25 +8,26 @@ from subprocess import check_output
 import sys
 
 
-def get_merged_branches():
+def get_remote_branches():
     ''' a list of merged branches, not couting the current branch or master '''
     raw_results = check_output('git branch ', shell=True)
+    for branch_name in raw_results.split('\n')
     print raw_results
-    return [b.strip() for b in raw_results.split('\n')
-			if b.strip() and not b.startswith('*') and (b.strip() != 'master' or b.strip() != 'dummy') ]
+    #~ return [b.strip() for b in raw_results.split('\n')
+			#~ if b.strip() and not b.startswith('*') and (b.strip() != 'master' or b.strip() != 'dummy') ]
 
 
-def delete_branch(branch):
-    return check_output('git branch -D %s' % branch, shell=True).strip()
+def merge_branch(branch):
+    return check_output('git merge master %s' % branch, shell=True).strip()
 
 
 if __name__ == '__main__':
     dry_run = '--confirm' not in sys.argv
-    for branch in get_merged_branches():
+    for branch in get_remote_branches():
         if dry_run:
             print branch
         else:
-            print delete_branch(branch)
+            print merge_branch(branch)
     if dry_run:
         print '*****************************************************************'
         print 'Did not actually delete anything yet, pass in --confirm to delete'
